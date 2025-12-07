e2eedc75063f191801b2fb03d433cbdcaa37eb4e
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2025-12-07 23:32:35 +0530

    Update priority_encoder_8.v

--- DAY7_Shifter_Encoder/priority_encoder_8.v
+++ DAY7_Shifter_Encoder/priority_encoder_8.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.12.2025 22:56:43
+// Design Name: 
+// Module Name: priority_encoder_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module priority_encoder_8 (
+    input  wire [7:0] in,
+    output reg  [2:0] out,
+    output reg        valid
+);
+    always @(*) begin
+        valid = |in;
+        casex (in)
+            8'b1xxxxxxx: out = 3'd7;
+            8'b01xxxxxx: out = 3'd6;
+            8'b001xxxxx: out = 3'd5;
+            8'b0001xxxx: out = 3'd4;
+            8'b00001xxx: out = 3'd3;
+            8'b000001xx: out = 3'd2;
+            8'b0000001x: out = 3'd1;
+            8'b00000001: out = 3'd0;
+            default:     out = 3'd0;
+        endcase
+    end
+endmodule
 
