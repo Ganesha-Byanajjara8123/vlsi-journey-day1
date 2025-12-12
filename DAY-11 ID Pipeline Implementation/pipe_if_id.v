c65e3d0de19d075f1108a14379e73a3f8925c9c2
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2025-12-12 18:02:45 +0530

    Update pipe_if_id.v

--- DAY-11 ID Pipeline Implementation/pipe_if_id.v
+++ DAY-11 ID Pipeline Implementation/pipe_if_id.v
@@ -1,1 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.12.2025 17:48:46
+// Design Name: 
+// Module Name: pipe_if_id
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pipe_if_id (
+    input        clk,
+    input        rstn,
+    input        stall,
+    input [7:0]  pc_in,
+    input [19:0] instr_in,
+    output reg [7:0]  pc_out,
+    output reg [19:0] instr_out
+);
+    always @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+            pc_out <= 0;
+            instr_out <= 0;
+        end
+        else if (!stall) begin
+            pc_out <= pc_in;
+            instr_out <= instr_in;
+        end
+    end
+endmodule
+
 
