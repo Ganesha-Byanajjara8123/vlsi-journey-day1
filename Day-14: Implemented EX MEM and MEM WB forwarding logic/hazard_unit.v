5b87d1970ac3e1605baddc08f37f61b913d7eb44
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2025-12-14 22:36:59 +0530

    Update hazard_unit.v

--- Day-14: Implemented EX MEM and MEM WB forwarding logic/hazard_unit.v
+++ Day-14: Implemented EX MEM and MEM WB forwarding logic/hazard_unit.v
@@ -1,1 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.12.2025 17:59:29
+// Design Name: 
+// Module Name: hazard_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module hazard_unit (
+    input [2:0] rs_id,
+    input [2:0] rt_id,
+    input [2:0] rd_ex,
+    input       regwrite_ex,
+    output      stall
+);
+    assign stall =
+        regwrite_ex &&
+        ((rd_ex == rs_id) || (rd_ex == rt_id)) &&
+        (rd_ex != 0);
+endmodule
+
 
