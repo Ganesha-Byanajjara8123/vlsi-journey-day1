c004d43a7541a6bcea7a5dcee9eeea949ae7cc9d
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2025-12-14 09:39:24 +0530

    Update forward_unit.v

--- Day-13: Pipeline Hazard Control & Forwarding Logic/forward_unit.v
+++ Day-13: Pipeline Hazard Control & Forwarding Logic/forward_unit.v
@@ -1,1 +1,16 @@
+module forwarding_unit (
+    input  [3:0] opcode_ex,
+    input  [3:0] opcode_prev,
+    output reg   forward_enable
+);
+
+    always @(*) begin
+        // simplistic hazard assumption
+        if (opcode_ex == opcode_prev)
+            forward_enable = 1'b1;
+        else
+            forward_enable = 1'b0;
+    end
+
+endmodule
 
