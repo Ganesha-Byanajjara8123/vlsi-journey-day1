cb3504b7d19304f3d283be9cc0ba875a965afe5e
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2025-12-16 22:27:15 +0530

    Update branch_unit.v

--- Day 16 – Pipeline Hazard Detection & Forwarding Integration/branch_unit.v
+++ Day 16 – Pipeline Hazard Detection & Forwarding Integration/branch_unit.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.12.2025 11:46:03
+// Design Name: 
+// Module Name: branch_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module branch_unit (
+    input  wire [7:0] A,
+    input  wire [7:0] B,
+    output wire       taken
+);
+    assign taken = (A == B);
+endmodule
+
 
